7954e27c0ed382e506f9260b639a48d3bdec1ce0
Rik Lenon <riklenon@gmail.com>
2021-08-04 21:49:56 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/riklenon/examplemongo/ExamplemongoApplication.java
@@ -0,0 +1,13 @@
+package com.riklenon.examplemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplemongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplemongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riklenon/examplemongo/ExamplemongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riklenon.examplemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplemongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
